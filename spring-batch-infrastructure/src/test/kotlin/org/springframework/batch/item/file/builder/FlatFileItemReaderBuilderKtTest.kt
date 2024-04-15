3a557d755c131effea220f501ec4827692c6ccdb
Gihwan Kim <ghkim3221@gmail.com>
2024-04-15 10:50:52 +0900

    Add Kotlin compiler plugin to tests

---
+++ spring-batch-infrastructure/src/test/kotlin/org/springframework/batch/item/file/builder/FlatFileItemReaderBuilderKtTest.kt
@@ -0,0 +1,11 @@
+package org.springframework.batch.item.file.builder
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class FlatFileItemReaderBuilderKtTest {
+    @Test
+    fun test() {
+        assertEquals(1, 2)
+    }
+}
